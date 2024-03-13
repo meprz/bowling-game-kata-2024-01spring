54718fa585c54b72d9f0484a28859ef7c3b1a113
Katie Bridson <kbridson@memphis.edu>
2024-03-13 16:09:44 -0500

    end of class

--- lib/bowling_game.rb
+++ lib/bowling_game.rb
@@ -1,11 +1,26 @@
 class BowlingGame
   def initialize
-    @score = 0
+    @rolls = Array.new(21, 0)
+    @current_roll = 0
   end
 
   def roll(pins)
-    @score += pins
+    @rolls[@current_roll] = pins
+    @current_roll += 1
   end
 
-  attr_reader :score
+  def score
+    score = 0
+    @rolls.each_with_index do |roll, i|
+      score += @rolls[i + 1] if at_end_of_frame?(i) && roll + @rolls[i - 1] == 10
+      score += roll
+    end
+    score
+  end
+
+  private
+
+  def at_end_of_frame?(i)
+    i.odd?
+  end
 end
--- spec/bowling_game_spec.rb
+++ spec/bowling_game_spec.rb
@@ -12,4 +12,14 @@ RSpec.describe BowlingGame do
     20.times { game.roll 1 }
     expect(game.score).to eq 20
   end
+
+  it 'should play a one-spare game' do
+    game.roll 5
+    game.roll 5
+    game.roll 3
+    17.times { game.roll 0 }
+    # first frame: 5 + 5 = 10 spare 10 + 3 = 13
+    # second frame: 13 + 3 + 0 = 16
+    expect(game.score).to eq 16
+  end
 end
